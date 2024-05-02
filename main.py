1dc0a8def6478f4c406615a00522fdbd194f2cda
Maks6666 <maxim.kucher2005@gmail.com>
2024-05-02 12:48:18 +0300

    1st task

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+# 1
+# При старті додатку запускаються три потоки. Перший
+# потік заповнює список випадковими числами. Два інші потоки
+# очікують на заповнення. Коли перелік заповнений, обидва
+# потоки запускаються. Перший потік знаходить суму елементів
+# списку, другий потік знаходить середнє арифметичне значення
+# у списку. Отриманий список, сума та середнє арифметичне
+# виводяться на екран.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import threading
+
+lst = []
+def full_list(lenght):
+    for i in range(lenght):
+        num = int(input(f"Input {i+1}st number: "))
+        lst.append(num)
+    print(lst)
+    return lst
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def lst_summ(lst):
+    el = 0
+    for i in lst:
+        el += i
+    print(f"Summa is {el}")
+
+def mid(lst):
+    el = 0
+    for i in lst:
+        el += i
+    mid_value = el/len(lst)
+    print(f"Medium value is {mid_value}")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+t1 = threading.Thread(target=full_list, args=(5, ))
+t1.start()
+t1.join()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if len(lst) > 0:
+    t2 = threading.Thread(target=lst_summ, args=(lst,))
+    t2.start()
+    t2.join()
+
+    t3 = threading.Thread(target=mid, args=(lst,))
+    t3.start()
+    t3.join()
+else:
+    raise Exception("List is empty")
